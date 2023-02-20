be2d5bc7b2416d8a67ea959a591aefd827585f09
“tianyazhang000” <1139549741@qq.com>
2023-02-20 23:46:55 +0800

    基本框架，还剩余controller、service

---
+++ gateway.go
@@ -0,0 +1,36 @@
+package douyin
+
+import (
+	"douyin/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter(r *gin.Engine) {
+	//设置静态目录为public文件夹
+	r.Static("/static", "./public")
+	r.Static("/favicon.ico", "/public/tinicon.ico.png")
+	apiRouter := r.Group("/douyin")
+
+	//// basic apis
+	//apiRouter.GET("/feed/", controller.Feed)
+	apiRouter.GET("/feed", controller.VideoFeed)
+	apiRouter.GET("/user/", controller.UserInfo)
+	apiRouter.POST("/user/register/", controller.Register)
+	apiRouter.POST("/user/login/", controller.Login)
+	apiRouter.POST("/publish/action/", controller.Publish)
+	apiRouter.GET("/publish/list/", controller.PublishList)
+	//
+	//// extra apis - I
+	apiRouter.POST("/favorite/action/", controller.FavoriteAction)
+	apiRouter.GET("/favorite/list/", controller.FavoriteList)
+	apiRouter.POST("/comment/action/", controller.CommentAction)
+	apiRouter.GET("/comment/list/", controller.CommentList)
+	//
+	//// extra apis - II
+	apiRouter.POST("/relation/action/", controller.RelationAction)
+	//关注列表
+	apiRouter.GET("/relation/follow/list/", controller.FollowerList)
+	//粉丝列表
+	apiRouter.GET("/relation/follower/list/", controller.BeFollowerList)
+	apiRouter.GET("/")
+}
---
+++ main.go
@@ -0,0 +1,22 @@
+package douyin
+
+import (
+	"douyin/public"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func main() {
+	//初始化数据库
+	if err := public.Init(); err != nil {
+		os.Exit(-1)
+	}
+	fmt.Println("数据库初始化完成")
+	r := gin.Default()
+	//路由初始化
+	InitRouter(r)
+	fmt.Println("路由初始化完成")
+	r.Run()
+	fmt.Println("路由启动")
+}
